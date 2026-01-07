6fed041ded111c1b2f5578fd345ba47a431f443b
sandeep-teotia <sandeepteotia50@gmail.com>
2026-01-07 11:09:13 +0530

    feat: Enable dynamic configuration refresh by adding Spring Cloud Config, configuring config server import, exposing refresh endpoints, and applying `@RefreshScope`.

--- cards/src/main/java/com/udemy/cards/controller/CardsController.java
+++ cards/src/main/java/com/udemy/cards/controller/CardsController.java
@@ -2,6 +2,7 @@ package com.udemy.cards.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import com.udemy.cards.service.ICardsService;
 
 @RestController
 @RequestMapping("/cards")
+@RefreshScope
 public class CardsController {
 
     @Autowired
--- loans/src/main/java/com/udemy/loans/controller/LoanController.java
+++ loans/src/main/java/com/udemy/loans/controller/LoanController.java
@@ -2,6 +2,7 @@ package com.udemy.loans.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import com.udemy.loans.service.ILoanService;
 
 @RestController
 @RequestMapping("/loans")
+@RefreshScope
 public class LoanController {
 
     @Autowired
